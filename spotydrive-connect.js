12962a9d5df748f904c68ad2436d1376f6bf87b1
Emerson Soares <on.emersonsoares@gmail.com>
2018-04-30 05:53:13 -0300

    Fixing connect

--- spotydrive-connect.js
+++ spotydrive-connect.js
@@ -1,4 +1,5 @@
 const program = require('commander')
+const path = require('path')
 const { fork } = require('child_process')
 
 program
@@ -8,7 +9,7 @@ program
 const oauth2 = require('./oauth2')(program.provider)
 console.log(oauth2.authorize())
 
-const child = fork('./callback-server')
+const child = fork(path.join(__dirname, './callback-server'))
 child.on('message', message => {
   oauth2.token(message)
   console.log('Login successful')
